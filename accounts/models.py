14fc312197b80c26c61c49e8d3b7df0c94015091
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2023-01-02 20:01:11 +0500

    setted templates

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=200,null=True)
-    phohe = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
     email = models.CharField(max_length=200,null=True)
     date_created = models.DateTimeField(auto_now_add=True,null=True)
     
