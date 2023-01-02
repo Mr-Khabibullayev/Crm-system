14c3eb6acba8535e794da91c22530faf1558e3e0
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2023-01-02 20:46:21 +0500

    added list in customer.html

--- accounts/views.py
+++ accounts/views.py
@@ -30,5 +30,11 @@ def products(request):
     products = Product.objects.all()
     return render(request , 'accounts/products.html',{'products': products})
 
-def customer(request):
-    return render(request , 'accounts/customer.html')
+def customer(request,pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    
+    orders = customer.order_set.all()
+    
+    context = {'customer': customer,'orders': orders}
+    
+    return render(request , 'accounts/customer.html',context)
