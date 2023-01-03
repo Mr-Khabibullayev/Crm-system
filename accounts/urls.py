9781b527c072976431ffae8962977c41e980324b
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2023-01-03 13:10:03 +0500

    added update and delete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from . import views
+from .views import (
+    home,
+    deleteOrder,
+    products,
+    customer,
+    createOrder,
+    updateOrder,
+)
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('products/',views.products,name='products'),
-    path('customer/<str:pk_test>',views.customer, name='customer' ),
+    path('',home,name='home'),
+    path('products/', products,name='products'),
+    path('customer/<str:pk_test>/',customer, name='customer' ),
+    
+    path('create_order/',createOrder,name='create_order'),
+    path('update_order/<str:pk>/',updateOrder,name='update_order'),
+    path('delete_order/<str:pk>/',deleteOrder,name='delete_order')
 ]
