b1d7dd0ae09a3d8263b46b2a48f252a8be616ad1
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2023-01-04 20:57:06 +0500

    added formset

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('products/', products,name='products'),
     path('customer/<str:pk_test>/',customer, name='customer' ),
     
-    path('create_order/',createOrder,name='create_order'),
+    path('create_order/<str:pk>/',createOrder,name='create_order'),
     path('update_order/<str:pk>/',updateOrder,name='update_order'),
     path('delete_order/<str:pk>/',deleteOrder,name='delete_order')
 ]
