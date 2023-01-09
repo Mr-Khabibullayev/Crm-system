69002a4cab87295beb4b67577e06f768c4748544
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2023-01-09 22:51:01 +0300

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from .views import (
     home,
     deleteOrder,
@@ -28,5 +31,14 @@ urlpatterns = [
     
     path('create_order/<str:pk>/',createOrder,name='create_order'),
     path('update_order/<str:pk>/',updateOrder,name='update_order'),
-    path('delete_order/<str:pk>/',deleteOrder,name='delete_order')
+    path('delete_order/<str:pk>/',deleteOrder,name='delete_order'),
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    path("reset_password_complete/",auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    
 ]
