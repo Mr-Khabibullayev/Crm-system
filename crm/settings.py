521cb0a4e5a7a99578550cfdc9b5cdb769cdf611
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2023-01-10 11:23:21 +0300

    added password_reset

--- crm/settings.py
+++ crm/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMIAL_HOST_USER = 'habibullayevaxrorbek34@gmail.com'
 EMAIL_HOST_PASSWORD = 'axrorbek1234567890'
